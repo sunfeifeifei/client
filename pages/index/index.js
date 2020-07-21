ec91cfea82d454f7185a8f3edc795655c10587b3
peggy7 <476978980@qq.com>
2020-07-21 09:39:04 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    list:[{
+      time:'昨日投放',
+      num:987
+    },
+    {
+      time: '2020-06-13',
+      num: 1021
+    }]
+  },
+  
+  onLoad () {
+    
+  },
+  
+})
